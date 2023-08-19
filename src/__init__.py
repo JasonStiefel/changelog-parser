fdbc74684fd98f7a00481df3a091721ad8cd678e
JasonStiefel <JasonStiefel@users.noreply.github.com>
2023-08-19 09:59:14 -0700

    Adding a dump example (#13)
    Co-authored-by: Jason Stiefel <RacecarBTF@Gmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ keepachangelog.com). In Python, the data is managed as a list of
 dictionaries (see README.md for the dictionaries' structure)
 """
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 import re
 import textwrap
